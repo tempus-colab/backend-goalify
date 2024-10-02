e23a3b134192a4ed73fc8bec44baf84a12994343
Stephen Asiedu <stephen.asiedu17@gmail.com>
2024-10-02 18:25:16 -0400

    added config.py, tested endpoints

--- models/user.py
+++
@@ -1,8 +0,0 @@
-from database import db
-from datetime import datetime
-
-class User(db.Model):
-    __tablename__ = 'users'  # Ensure this matches the MySQL table name
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)  
