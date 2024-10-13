07f59fd3e719f421f98b8b1869255f66b472c29c
Hemense Lan <lihemen@gmail.com>
2024-10-13 22:29:47 +0100

    remove create app configuration

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def blueprint_config(app):
 
 
 if __name__ == '__main__':
-    app = create_app('DevelopmentConfig')
+    app = create_app()
     blueprint_config(app)
     with app.app_context():
         db.create_all()
